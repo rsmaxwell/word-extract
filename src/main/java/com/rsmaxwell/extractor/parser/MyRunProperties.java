967d45dfa0424ca5f7f43f6c6b4b21c991d5f0ad
Richard Maxwell <richard@rsmaxwell.co.uk>
2020-01-24 17:52:59 +0000

    allow a w:lang tag in a RunProperties element

--- src/main/java/com/rsmaxwell/extractor/parser/MyRunProperties.java
+++ src/main/java/com/rsmaxwell/extractor/parser/MyRunProperties.java
@@ -45,6 +45,8 @@ public class MyRunProperties extends MyElement {
 					// ok
 				} else if ("w:kern".contentEquals(nodeName)) {
 					// ok
+				} else if ("w:lang".contentEquals(nodeName)) {
+					// ok
 				} else {
 					throw new Exception("unexpected element: " + nodeName);
 				}
