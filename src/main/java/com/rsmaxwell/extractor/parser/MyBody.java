c7ad5d5504e6439f8527b48d9b34c55778bb67ac
Richard Maxwell <richard@rsmaxwell.co.uk>
2020-02-15 21:04:17 +0000

    replace special chars with htmp4 entities

--- src/main/java/com/rsmaxwell/extractor/parser/MyBody.java
+++ src/main/java/com/rsmaxwell/extractor/parser/MyBody.java
@@ -84,6 +84,7 @@ public class MyBody {
 				String string = element.toString();
 				try {
 					Extractor extractor = Extractor.instance;
+					string = string.replaceAll("\\<.*?\\>", "");
 					extractor.month = Month.toInt(string);
 
 				} catch (Exception e) {
